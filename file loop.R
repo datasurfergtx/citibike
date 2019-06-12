e7b345a0f601240104a8cb3505aca39f52b0ec01
Jacky Lam <Jacky@Jackys-MBP.home>
2019-06-11 23:56:18 -0400

    timezone changes to loop

--- file loop.R
+++ file loop.R
@@ -79,22 +79,6 @@ sum(is.na(a))
 trips2015$stoptime <- a
 
 
-
-
-
-#load in 2016 data####
-setwd("~/Documents/R/temp/2016")
-trips2016 = pblapply(list.files(pattern="*\\.csv"), function(x){
-  fread(x) 
-})%>% rbindlist()
-
-
-
-
-
-
-
-
 #clean up data####
 
 # trips$starttime = fastPOSIXct(trips$starttime)
@@ -132,3 +116,80 @@ setwd("~/Documents/R/citibike/fst")
 test = read.fst("citi-2013-07-01.fst")
 test2 = read.fst("citi-2015-01-01.fst")
 test3 = rbind(test,test2)
+
+
+
+
+
+
+#load in 2016 and 2017 data####
+# setwd("~/Documents/R/temp/2016")
+setwd("~/Downloads/combined")
+
+trips = pblapply(list.files(pattern="*\\.csv"), function(x){
+  fread(x) 
+})%>% rbindlist(use.names = FALSE)
+
+#fixing two datetimes in one column####
+# a = parse_date_time(trips$starttime, orders = 'mdy HMS')
+# b = parse_date_time(trips$starttime, orders = 'ymd HMS')
+
+a = ymd_hms(trips$starttime)
+b = mdy_hms(trips$starttime)
+
+# table(months(a), year(a))
+# table(months(b), year(b))
+
+#check if our code worked
+sum(is.na(a)) + sum(is.na(b))
+
+a[is.na(a)] <- b[!is.na(b)]
+sum(is.na(a))
+trips$starttime <- a
+sum(is.na(trips$starttime))
+
+
+#trip stoptime
+
+a = ymd_hms(trips$stoptime)
+b = mdy_hms(trips$stoptime)
+
+#check if our code worked
+sum(is.na(a)) + sum(is.na(b))
+
+a[is.na(a)] <- b[!is.na(b)]
+sum(is.na(a))
+trips$stoptime <- a
+
+#import to check data types
+setwd("~/Documents/R/citibike/fst")
+test = read.fst("citi-2013-07-01.fst")
+#we need  end station id as num, fix birth year and gender.
+
+
+#clean up data####
+
+trips$`end station id` = as.numeric(trips$`end station id`)
+trips$`birth year` = as.numeric(trips$`birth year`)
+
+#recoding male to 0 and female to 1 unknown is now NA
+trips$gender = gsub(0,NA,trips$gender)
+trips$gender = gsub("1","0",trips$gender)
+trips$gender = gsub("2","1",trips$gender)
+trips$gender = as.numeric(trips$gender)
+
+
+#loop to create daily fst files####
+# setwd("C:\\Users\\lamja\\Documents\\R\\citibike\\fst")
+setwd("~/Documents/R/citibike/fst")
+
+trips$date = as.Date(substr(trips$starttime,1,10))
+
+x = seq.Date(as.Date('2016-01-01'),as.Date('2017-12-31'), by="day")
+
+
+for (i in x){
+  a <- trips %>% dplyr::filter(date == i)
+  a$date = NULL
+  write.fst(a,paste0("citi-",as.Date(i),".fst"))
+}
